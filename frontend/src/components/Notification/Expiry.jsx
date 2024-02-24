e3eb916a84f617133eaee5c86ff5b45c0cf7ed15
RavinduSM <ravindumuthukudaarachchi@gmail.com>
2024-02-24 15:28:59 +0530

    updated

--- frontend/src/components/Notification/Expiry.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Expiry() {
-  return (
-    <div>Expiry</div>
-  )
-}
-
-export default Expiry
---
+++ index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import driverRouter from './routes/driverRouter.js';
+import uploadRouter from './routes/uploadRouter.js'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+})
+
+const app = express();
+app.use(express.json());
+// Enable cors at the server side. 
+
+const corsOption = {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}
+app.use(cors(corsOption));
+
+app.use('/api/driver', driverRouter);
+app.use('/api/upload', uploadRouter);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+
+const port = process.env.port || 5000;
+app.listen(port, () => {
+    console.log(`Serve at http:localhost:${port}`)
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+})
